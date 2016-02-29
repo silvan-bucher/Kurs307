f096986ace8c1fbd456c966754a43264471103fb
silvan-bucher <silvan.bucher99@gmail.com>
2016-02-29 13:16:35 +0100

    Adding Step 4 to Beer excercise

--- Aufgaben/Beer/script.php
+++ Aufgaben/Beer/script.php
@@ -11,7 +11,11 @@ function getWord($bottleCount){
       return $bottleCount . " bottles";
   }
 }
+
 for($bottleCount = 99; $bottleCount > 0; $bottleCount--){
   echo getWord($bottleCount) . " of beer on the wall, " . getWord($bottleCount) . " of beer.\n";
   echo "Take one down and pass it around, " . getWord($bottleCount - 1) . " of beer on the wall.\n\n";
 }
+
+echo "No more bottles of beer on the wall, no more bottles of beer.\n";
+echo "Go to the store and buy some more, 99 bottles of beer on the wall.";
